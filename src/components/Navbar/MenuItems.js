228cfd0d76750c3417df6fd61c752acd4e5099a6
Jose Lopez <joselopez11394@gmail.com>
2021-03-13 16:54:46 -0500

    adds the navbar to the page and starting on the home page

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,22 @@
+export const MenuItems = [
+  {
+    title: 'Home',
+    url: "/",
+    cName: 'nav-links'
+  },
+  {
+    title: 'Skills',
+    url: "/skills",
+    cName: 'nav-links'
+  },
+  {
+    title: 'Projects',
+    url: "/projects",
+    cName: 'nav-links'
+  },
+  {
+    title: 'Work Experience',
+    url: "/works",
+    cName: 'nav-links'
+  },
+]
