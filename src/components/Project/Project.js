61b804ec18c28c2f593b01f324acfae58c2001e7
Jose Lopez <joselopez11394@gmail.com>
2021-05-11 18:13:45 -0400

    starting to use semantic ui instead of css and reworks the navbar

---
+++ src/components/Project/Project.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './Project.css'
+
+class Project extends Component {
+  render() {
+    return(
+      <>
+        <div className="project-one">
+          <h1>Pack Smart</h1>
+            
+        </div>
+      </>
+    )
+  }
+}
+
+export default Project
